7b5e0939341899b8b3ddcbae46eb6333a730bf0f
Nathan H <113381295+Fisail@users.noreply.github.com>
2025-01-25 16:44:01 +0100

    Update FamilyRepository.kt

--- products/src/main/kotlin/iut/nantes/project/products/Repository/FamilyRepository.kt
+++ products/src/main/kotlin/iut/nantes/project/products/Repository/FamilyRepository.kt
@@ -4,5 +4,5 @@ import iut.nantes.project.products.Entity.FamilyEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface FamilyRepository : JpaRepository<FamilyEntity, String> {
-    fun existsByName(name: String): Boolean
+    fun existsByName(name: String): Boolean //A IMPLEMENTER
 }
